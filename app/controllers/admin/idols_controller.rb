f2062738be1eb50f3057787f45a781b82339be75
sugyan <sugi1982@gmail.com>
2015-05-04 17:53:31 +0900

    rename

--- app/controllers/admin/idols_controller.rb
+++ app/controllers/admin/idols_controller.rb
@@ -1,26 +1,26 @@
 # coding: utf-8
-class Admin::IdolController < AdminController
+class Admin::IdolsController < AdminController
   before_action :set_idol, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/idol
+  # GET /admin/idols
   def index
     @idols = Idol.order(id: :desc)
   end
 
-  # GET /admin/idol/1
+  # GET /admin/idols/1
   def show
   end
 
-  # GET /admin/idol/new
+  # GET /admin/idols/new
   def new
     @idol = Idol.new
   end
 
-  # GET /admin/idol/1/edit
+  # GET /admin/idols/1/edit
   def edit
   end
 
-  # POST /admin/idol
+  # POST /admin/idols
   def create
     @idol = Idol.new(idol_params)
     if @idol.save
@@ -30,7 +30,7 @@ class Admin::IdolController < AdminController
     end
   end
 
-  # PATCH/PUT /admin/idol/1
+  # PATCH/PUT /admin/idols/1
   def update
     if @idol.update(idol_params)
       redirect_to editor_idol_url(@idol), notice: "更新されました"
@@ -39,10 +39,10 @@ class Admin::IdolController < AdminController
     end
   end
 
-  # DELETE /admin/idol/1
+  # DELETE /admin/idols/1
   def destroy
     @idol.destroy
-    redirect_to editor_idol_index_url, notice: "削除されました"
+    redirect_to editor_idols_url, notice: "削除されました"
   end
 
   private
