a674817581df08a565d0f6233eb565f4274bdb90
sugyan <sugi1982@gmail.com>
2015-05-27 00:43:27 +0900

    oops..

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     u = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
+      user.nickname = auth.info.nickname
     end
     # nickname(screen_name)は変わる可能性ある
     u.update_attributes(nickname: auth.info.nickname)
