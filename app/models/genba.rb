7458a01ff4c2b9e030d7a9ffdf8d999e317f6db0
sugyan <sugi1982@gmail.com>
2015-05-28 01:00:37 +0900

    update genba/detail page

--- app/models/genba.rb
+++ app/models/genba.rb
@@ -3,7 +3,7 @@ class Genba < ActiveRecord::Base
   extend ObfuscateId::ClassMethods
   obfuscate_id spin: obfuscate_id_default_spin + ENV['OBFUSCATE_SPIN'].to_i
 
-  has_and_belongs_to_many :idols
+  has_and_belongs_to_many :idols, ->{ order(:kana) }
   belongs_to :location
 
   serialize :urls, Array
@@ -18,23 +18,6 @@ class Genba < ActiveRecord::Base
       location: self.location.name,
       idols: self.idols.map{ |idol| idol.serializable_hash(only: [:name, :kana]) },
       link:  Rails.application.routes.url_helpers.genba_path(self),
-    )
-  end
-
-  def attributes_for_detail
-    idols = self.idols.order(:kana).map do |idol|
-      idol.serializable_hash(only: [:name]).merge(
-        id: idol.to_param,
-        link: Rails.application.routes.url_helpers.idol_path(idol),
-      )
-    end
-    location = self.location.serializable_hash(only: [:name, :address]).merge(
-      link: Rails.application.routes.url_helpers.location_path(self.location),
-    )
-    self.serializable_hash(only: [:title, :description, :more_idols, :urls]).merge(
-      start_at: I18n.l(self.start_at, format: '%Y年%-m月%-d日(%a) %H:%M'),
-      location: location,
-      idols: idols,
     )
   end
 
